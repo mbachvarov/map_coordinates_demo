88599fa7de2d2832ce2281bcf433adb526c24b95
Momchil Bachvarov <momchilbuchvarov@abv.bg>
2020-08-31 12:04:37 +0300

    Get address coordinates reorganisation

--- src/get_address_coordinates.php
+++ src/get_address_coordinates.php
@@ -5,32 +5,40 @@
 	use Api\Map\Coordinates\MapCoordinatesAPIContainer;
 	use Api\Map\Coordinates\BaseMapCoordinatesAPI;
 	
-	if(isset($_GET['address'])){
-		$address = $_GET['address'];
-		
+	if(isset($_GET['address'])) {
+		echo findCoordinatesOf($_GET['address']);
+	} else {
+		echo json_encode(['error'=>'Missing address param']);
+	}	
+
+	function findCoordinatesOf($address) {	
+		$apiContainer = initializeApiContainer();
+
+	    $mapCoordinatesFinder = new MapCoordinatesFinder($apiContainer);
+	    $mapCoordinatesFinder->findCoordinatesOf($address);
+
+	    return json_encode($mapCoordinatesFinder->getResults());    	
+	}
+
+	function initializeAPIContainer() {
 		$apiContainer = new MapCoordinatesAPIContainer;
 	    $defaultAPIsConfigs =  include(__DIR__.'/../config/map_coordinates_api/config.php');
 	    foreach ($defaultAPIsConfigs as $apiConfig) {
-	    	try{
+	    	try {
 				$api = new BaseMapCoordinatesAPI(include($apiConfig['config_dir']));
 				$api->setParser($apiConfig['parser']);
 			    $apiContainer->add($api);
-			}catch(\InvalidArgumentException $e) {
+			} catch(\InvalidArgumentException $e) {
 				$configDir = $apiConfig['config_dir'];
 				echo json_encode(['error'=>"Map Coordinates API invalid config: $configDir"]);
 				exit;
-			} catch(\TypeError $e){
+			} catch(\TypeError $e) {
 				$configDir = $apiConfig['config_dir'];
 				echo json_encode(['error'=>"Map Coordinates API config not found: $configDir"]);
 				exit;	
 			}
 	    }
 
-	    $mapCoordinatesFinder = new MapCoordinatesFinder($apiContainer);
-	    $mapCoordinatesFinder->findCoordinatesOf($address);
-
-	    echo json_encode($mapCoordinatesFinder->getResults());    	
-	}else{
-		echo json_encode(['error'=>'Missing address param']);
-	}	
+	    return $apiContainer;
+	}
 ?>
